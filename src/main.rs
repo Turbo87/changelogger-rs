5afa1498e00e99edd245fe56ff9f984266ecfb5b
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-18 20:47:24 +0100

    Add basic `clap` structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::App;
+use clap::{crate_name, crate_version, crate_description};
+
 fn main() {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .get_matches();
+
     println!("Hello, world!");
 }
