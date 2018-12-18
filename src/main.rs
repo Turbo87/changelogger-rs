5bf1dd42eed66dbfa440ce738bdae369aa74d5ff
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-18 21:38:02 +0100

    Add optional `PATH` argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use clap::App;
+use std::env;
+
+use clap::{App, Arg};
 use clap::{crate_name, crate_version, crate_description};
 
-fn main() {
-    App::new(crate_name!())
+fn main() -> std::io::Result<()> {
+    let path = env::current_dir()?.into_os_string();
+
+    let matches = App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
+        .arg(Arg::with_name("PATH")
+            .help("path of a git repository")
+            .index(1)
+            .default_value_os(path.as_os_str()))
         .get_matches();
 
-    println!("Hello, world!");
+    let path = matches.value_of("PATH").unwrap();
+    println!("PATH: {}", path);
+
+    Ok(())
 }
